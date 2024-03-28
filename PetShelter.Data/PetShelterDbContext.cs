f03d2f31deed2c4fdcc5c56ef483ed3be76ba13b
Gabsana-12 <gabunivanov@gmail.com>
2024-03-28 09:24:31 +0200

    I added project PetShelterMVC

---
+++ PetShelter.Data/PetShelterDbContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PetShelter.Data
+{
+    class PetShelterDbContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+
+        public PetShelterDbContext(DbContextOptions<PetShelterDbContext> options) : base(options)
+        {
+
+        }
+    }
+}

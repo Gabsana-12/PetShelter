06406ceca232159e109573143349aaba54d0352a
pmg <gabunivanov@gmail.com>
2024-06-05 10:04:15 +0300

    Worked on html

--- PetShelterMVC/ViewModel/BreedDetailsVM.cs
+++ PetShelterMVC/ViewModel/BreedDetailsVM.cs
@@ -9,6 +9,7 @@ namespace PetShelterMVC.ViewModel
     public class BreedDetailsVM : BaseVM
     {
         public string Name { get; set; }
+        public int Id { get; set; }
         public BreedSize Size { get; set; }
         public List<PetDetailsVM> Pets { get; set; }
     }
